dedb8120c52a41bd03775ac46f664793b2f591e0
jmframe <jmframe@crimson.ua.edu>
2022-06-21 05:51:26 +0000

    bucket model runs

--- bucket/bucket.py
+++ bucket/bucket.py
@@ -8,69 +8,77 @@ class BUCKET():
         
     # __________________________________________________________________________________________________________
     # MAIN MODEL FUNCTION
-    def run_bucket(self, bucket_state):
+    def run_bucket(self, b):
+        """
+            This is a simple model of a bucket with a hole (outlet) in it
+            Args:
+                self (class): This is the model class
+                b (dictionary): This has all the information to run the model, including fluxes.
+                    input_m
+                    water_level_m
+                    max_water_surface_elevation_m
+                    potential_et_m_per_timestep
+                    actual_et_m_per_timestep
+
+                    total_in_m3
+                    total_lost_m3
+            Returns:
+                Nothing, just updates the values in the b
+        """
         
         # ________________________________________________
-        bucket_state.volin += bucket_state.timestep_input_m
+        b.total_in += b.input_m 
+       
+       # Add the input mass to the bucket
+        b.water_level_m = b.water_level_m + b.input_m
+        
         
         # ________________________________________________
-        bucket_state.potential_et_m_per_timestep = bucket_state.potential_et_m_per_s * bucket_state.time_step_size
+        b.potential_et_m_per_timestep = b.potential_et_m_per_s * b.time_step_size
         
-
-        # Add the input mass to the bucket
-        bucket_state.h_water_level = bucket_state.h_water_level + bucket_state.volin
-
-        # Lose mass out of the bucket. Some periodic type loss, evaporation, and some infiltration...
-        # ________________________________________________
-        # SUBROUTINE
-        # timestep_rainfall_input_m = f(timestep_rainfall_input_m, potential_et_m_per_timestep)
-        self.et_from_bucket(bucket_state)
+        b.actual_et_m_per_timestep = np.min(np.array([b.potential_et_m_per_timestep, b.water_level_m]))
         
-
+        b.water_level_m = b.water_level_m - b.actual_et_m_per_timestep
+        
+        b.total_lost += b.actual_et_m_per_timestep
+        
 
         # Overflow if the bucket is too full
-        if h_water_level[ibuc] > h_max[ibuc]:
-            mass_overflow[ibuc] = h_water_level[ibuc] - h_max[ibuc]
-            h_water_level[ibuc] = h_max[ibuc] - np.random.uniform(0, noise['h2'])
-
-        # Calculate head on the spigot
-        h_head_over_spigot = (h_water_level[ibuc] - h_spigot[ibuc] ) * np.random.normal(1, noise['head'])
-
-        # Calculate water leaving bucket through spigot
-        if h_head_over_spigot > 0:
-            velocity_out = np.sqrt(2 * g * h_head_over_spigot)
-            spigot_out = velocity_out *  spigot_area[ibuc] * time_step
-            h_water_level[ibuc] = h_water_level[ibuc] - spigot_out
+        if b.water_level_m > b.max_water_surface_elevation_m:
+            # TODO: ADD WEIR EQUATION
+            b.overflow_m = b.water_level_m - b.max_water_surface_elevation_m
         else:
-            spigot_out = 0
+            b.overflow_m = 0
+        b.overflow_m3 = b.overflow_m * b.bucket_top_area_m2
+        b.total_overflow += b.overflow_m
 
+        # Calculate head on the outlet
+        b.head_over_outlet_m = (b.water_level_m - b.outlet_m)
 
+        # Calculate water leaving bucket through outlet
+        if b.head_over_outlet_m > 0:
+            
+            b.velocity_out_m_per_s = np.sqrt(2 * b.g * b.head_over_outlet_m)
+            
+            b.outlet_m3 = b.velocity_out_m_per_s *  b.outlet_cross_area_m2 * b.time_step_size
+            b.outlet_m = b.outlet_m3 / b.bucket_top_area_m2
+        
+        else:
+
+            b.outlet_m3= 0
+            b.outlet_m = 0
+        
+        # ________________________________________________
+        b.water_level_m = b.water_level_m - b.outlet_m - b.overflow_m
 
         # ________________________________________________
-        cfe_state.current_time_step += 1
-        cfe_state.current_time      += pd.Timedelta(value=cfe_state.time_step_size, unit='s')
+        b.total_outlet += b.outlet_m3
+        b.total_overflow += b.overflow_m3
+
+        # ________________________________________________
+        b.current_time_step += 1
+        b.current_time      += pd.Timedelta(value=b.time_step_size, unit='s')
 
         return
     
 
-    # __________________________________________________________________________________________________________
-    def et_from_bucket(self,bucket_state):
-        
-        """
-            open water loses to the atmosphere.
-
-        """
-        
-        if bucket_state.input_m >0.0:
-
-            if bucket_state.timestep_input_m > bucket_state.potential_et_m_per_timestep:
-        
-                bucket_state.actual_et_m_per_timestep = bucket_state.potential_et_m_per_timestep
-                bucket_state.timestep_rainfall_input_m -= bucket_state.actual_et_m_per_timestep
-
-            else: 
-
-                bucket_state.potential_et_m_per_timestep -= cfe_state.timestep_rainfall_input_m
-                cfe_state.timestep_rainfall_input_m=0.0
-        return
-                
